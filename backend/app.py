2d7e083408b3f6189b13e00ebc231a0334b0bb39
Adam Zernik <adam.zernik@gmail.com>
2022-03-15 17:18:37 +0200

    updated data & backend

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,26 @@
+import json
 from flask import Flask
 import pandas as pd
 from random import uniform
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def live():
     return "live"
 
+
+def randomize_payments(records):
+    random_coefficient = 1 + uniform(-0.1, 0.1)
+    df = pd.DataFrame.from_dict(records)
+    for col in [c for c in df.columns if c.startswith('tenant_')]:
+        df[col] = df[col].apply(lambda x: round(x * random_coefficient, 2))
+    return json.loads(df.to_json(orient='records'))
+
+
 @app.route('/get_dataset', methods=['GET'])
 def get_dataset():
-    df = pd.read_csv('./frontend_interview_dataset - payments.csv')
-    df.payment_amt = df.payment_amt.apply(lambda x: x * (1 + uniform(-0.1, 0.1)))
-    return df.to_json()
+    data = json.loads(open('./data.json').read())
+    data['payments'] = randomize_payments(data['payments'])
+    return data
